4275272296a135757c9e184cda8688d343f0101d
CoBeTcKuu <9437855@gmail.com>
2022-08-16 21:16:12 +0300

    коммит 1 тест из шпаргалки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        /* int[] arr = {1, 2, 3, 4, 5};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        char[][] gameField = new char[3][3];
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[0].length; j++) {
+                System.out.print(gameField[i][j] + " ");
+            }
+            System.out.println();
+        } */
+        // задача 1
 
     }
 }
